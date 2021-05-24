f63ac2fe0c82a8bdafc965f59dbef94123abb026
MahammadKasim Nadim <Mahammad.nadim@mindtree.com>
2021-05-24 20:48:15 +0530

    text field created

---
+++ src/components/FormFields/TextFormFields.tsx
@@ -0,0 +1,18 @@
+import { FieldProps, getIn } from "formik";
+import { TextField } from "@material-ui/core";
+
+export const TextFormField: React.FC<FieldProps> = ({ field, form, ...props }) => {
+    const errorText =
+        getIn(form.touched, field.name) && getIn(form.errors, field.name);
+
+    return (
+        <TextField
+            fullWidth
+            margin="normal"
+            helperText={errorText}
+            error={!!errorText}
+            {...field}
+            {...props}
+        />
+    );
+};
