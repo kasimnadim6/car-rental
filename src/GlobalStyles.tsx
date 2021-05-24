88e138cc5a2ae714cc81e250b23d7f212705a21d
MahammadKasim Nadim <Mahammad.nadim@mindtree.com>
2021-05-24 13:17:31 +0530

    global style creation

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+        font-family: 'Nobile', sans-serif !important;
+    }
+    html{
+        font-size:62.5%;
+    }
+`;
